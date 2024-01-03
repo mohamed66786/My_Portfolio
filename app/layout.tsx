5ecb940f5a74e4bd1be54ffcef3c15b96c322e44
mohamed66786 <mohamedtarek7080@gmail.com>
2024-01-03 20:43:36 +0200

    add header and info components and add new features

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,36 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import Intro from "@/components/intro";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'MohamedTarek | Personal Portfolio',
-  description: 'Full Stack Developer',
-}
+  title: "MohamedTarek | Personal Portfolio",
+  description: "Full Stack Developer",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}</body>
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36
+          h-[1950px]`}
+      >
+        <div className="bg-[#f0dada] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem]
+         w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
+        <div className="bg-[#ccc7f2] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem]
+         w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem]
+          2xl:left-[-5rem] "></div>
+          <Header/>
+          <Intro/>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
